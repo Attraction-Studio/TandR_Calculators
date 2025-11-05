8d9fd05f534fa3ef3409ae3d7693e7e025407a7f
DavidWaters-Dev <david@davidwaters.pro>
2025-11-05 13:53:26 +1300

    commit to fix env

--- ceiling-components-estimator/vite.config.js
+++ ceiling-components-estimator/vite.config.js
@@ -4,6 +4,9 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    'process.env': {}
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.js'),
