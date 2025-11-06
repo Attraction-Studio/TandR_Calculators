944b74ae29a1bcad9c6f3421cdcf6fc0f1074096
DavidWaters-Dev <david@davidwaters.pro>
2025-11-07 11:00:23 +1300

    removed max width and padding as controleed bia webflow

--- calculators-app/src/App.vue
+++ calculators-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+    <div class="w-full py-8">
       <!-- Calculator Navigation -->
       <CalculatorNav
         :calculators="calculators"
