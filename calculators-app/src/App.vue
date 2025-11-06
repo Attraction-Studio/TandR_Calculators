179bd341d2042fe9859e0f3b085607aa791c5102
DavidWaters-Dev <david@davidwaters.pro>
2025-11-07 10:57:06 +1300

    removed bg-white from app

--- calculators-app/src/App.vue
+++ calculators-app/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-white">
+  <div class="min-h-screen">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <!-- Calculator Navigation -->
       <CalculatorNav
@@ -17,53 +17,52 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
-import CalculatorNav from './components/CalculatorNav.vue';
-import CeilingComponentsEstimator from './calculators/CeilingComponentsEstimator.vue';
+  import { ref, computed } from "vue";
+  import CalculatorNav from "./components/CalculatorNav.vue";
+  import CeilingComponentsEstimator from "./calculators/CeilingComponentsEstimator.vue";
 
-// Calculator registry
-const calculators = [
-  {
-    id: '1',
-    name: 'Ceiling Components Estimator',
-    component: CeilingComponentsEstimator,
-    available: true,
-  },
-  {
-    id: '2',
-    name: 'Seismic Calculator',
-    component: null,
-    available: false,
-  },
-  {
-    id: '3',
-    name: 'Seismic Calculator - Plasterboard',
-    component: null,
-    available: false,
-  },
-  {
-    id: '4',
-    name: 'Seismic Calculator - Baffle Ceilings',
-    component: null,
-    available: false,
-  },
-];
+  // Calculator registry
+  const calculators = [
+    {
+      id: "1",
+      name: "Ceiling Components Estimator",
+      component: CeilingComponentsEstimator,
+      available: true,
+    },
+    {
+      id: "2",
+      name: "Seismic Calculator",
+      component: null,
+      available: false,
+    },
+    {
+      id: "3",
+      name: "Seismic Calculator - Plasterboard",
+      component: null,
+      available: false,
+    },
+    {
+      id: "4",
+      name: "Seismic Calculator - Baffle Ceilings",
+      component: null,
+      available: false,
+    },
+  ];
 
-// Active calculator state
-const activeCalculator = ref('1');
+  // Active calculator state
+  const activeCalculator = ref("1");
 
-// Get active calculator component
-const activeComponent = computed(() => {
-  const calc = calculators.find((c) => c.id === activeCalculator.value);
-  return calc?.component || null;
-});
+  // Get active calculator component
+  const activeComponent = computed(() => {
+    const calc = calculators.find((c) => c.id === activeCalculator.value);
+    return calc?.component || null;
+  });
 
-// Select calculator
-function selectCalculator(id) {
-  const calc = calculators.find((c) => c.id === id);
-  if (calc && calc.available) {
-    activeCalculator.value = id;
+  // Select calculator
+  function selectCalculator(id) {
+    const calc = calculators.find((c) => c.id === id);
+    if (calc && calc.available) {
+      activeCalculator.value = id;
+    }
   }
-}
 </script>
-
